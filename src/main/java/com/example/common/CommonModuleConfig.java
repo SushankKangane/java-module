acf707500387e8c96a36acd371770a9d03b20105
sushankkangane <Kanganesushank36@gmail.com>
2024-06-08 19:37:42 +0530

    added main class [optional]

--- src/main/java/com/example/common/CommonModuleConfig.java
+++ src/main/java/com/example/common/CommonModuleConfig.java
@@ -6,16 +6,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@SpringBootApplication
-//public class CommonApplication {
-//	public static void main(String[] args) {
-//		SpringApplication.run(CommonApplication.class, args);
-//	}
-//
-//}
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.example.common.repository")
-@EntityScan(basePackages = "com.example.common.entity")
+@SpringBootApplication
 public class CommonModuleConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonModuleConfig.class, args);
+
+    }
 }
